6798965666e297b16f4a62329f27d057474e0737
jootaha <yousseftaha823@gmail.com>
2026-01-23 20:15:29 +0200

    created the stockMovement model

---
+++ backend/enums/stockMovement.enum.js
@@ -0,0 +1,8 @@
+const STOCK_MOVEMENT_TYPE = Object.freeze({
+  RETURN: "return",
+  BATCH: "batch",
+  SALES: "sales",
+  MANUAL_ADJUSTMENT: "manual_adjustment",
+});
+
+module.exports = STOCK_MOVEMENT_TYPE;
---
+++ backend/models/stockMovement.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const STOCK_MOVEMENT_TYPE = require("../enums/stockMovement.enum");
+
+const stockMovementSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    quantityChange: {
+      type: Number,
+      required: true,
+    },
+
+    movementType: {
+      type: String,
+      enum: Object.values(STOCK_MOVEMENT_TYPE),
+      required: true,
+    },
+
+    movementTime: {
+      type: Date,
+      default: Date.now, // business time when stock actually changed
+    },
+
+    notes: {
+      type: String,
+      trim: true,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // createdAt & updatedAt (system audit)
+  }
+);
+
+const StockMovement = mongoose.model("StockMovement", stockMovementSchema);
+module.exports = StockMovement;
